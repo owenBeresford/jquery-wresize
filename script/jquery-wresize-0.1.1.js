a6fcaf4f30c0c92dddd5c781fdd8032801a328cf
owen beresford <owenberesford@fastmail.fm>
2015-01-17 19:48:20 +0000

    Notice that the error warning you about old MSIE used console.log() :-)

--- script/jquery-wresize-0.1.1.js
+++ script/jquery-wresize-0.1.1.js
@@ -63,7 +63,7 @@ PS, on FF this works best attached to the window, not the document.  Its a brows
 // msie 8
 		if (window.attachEvent && !window.addEventListener) {
 			if(options.debug) {
-				console.log("wresize() WARNING: Ancient browser, hopefully everything still computes.");
+				alert("wresize() WARNING: Ancient browser, hopefully everything still computes.\n(Dev - import es5-shim before this module to avoid this message).");
 			}
 
 		}
